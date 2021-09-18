af420b90d922917044dd7ffd1bfea0057f7728a9
suhasini <suhasini@suhasini-VirtualBox>
2021-09-18 19:51:52 +0530

    my first commit

---
+++ test-repo/src/main/java/Addition2nos.java
@@ -0,0 +1,11 @@
+
+public class Addition2nos {
+	public static void main(String[] args) {
+		int a=2;
+		int b=3;
+		int c=0;
+		c=a+b;
+		System.out.println("addition 2 nos"+c);
+	}
+
+}
---
+++ test-repo/src/test/java/Additiontest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Additiontest {
+
+	@Test
+	public void test() {
+		System.out.println("test case pass");
+		
+	}
+
+}
